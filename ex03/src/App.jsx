6c24cc434561c3fef75543d83c68557f9cc81f79
LeonardoRibeiro-cpu <leonardo.ribeiro@estudante.iftm.edu.br>
2026-04-21 02:56:47 -0300

    Criar um repositorio

---
+++ ex03/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Cards/card";
+
+import "./css/App.css";
+function App() {
+    return (
+         <div className="container">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card/>
+        </div>
+        
+    );
+}
+
+export default App;
---
+++ ex03/src/Cards/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return ( 
+        <div className="card">
+            <h3>Titula da caixa</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sapiente fugit, maxime temporibus cumque omnis aliquid nesciunt eveniet laudantium, quam veniam neque ea in accusamus nisi aperiam facere corrupti doloribus..</p>
+        </div>
+     );
+}
+
+export default Card;
